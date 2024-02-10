0c2e090ff31e27c8279516d421bb84775e1843d9
Mominur Bashar Ovi <bashar.ovi@outlook.com>
2024-02-10 10:42:14 +0600

    updated code

--- src/Chai.js
+++ src/Chai.js
@@ -1,5 +1,10 @@
 function HeyChai() {
-  return <h4 className="App-logo"> Hello React! </h4>;
+  return (
+    <>
+      <h4 className="App-logo"> Hello React! </h4>
+      <p> This is a simple React app. </p>
+    </>
+  );
 }
 
 export default HeyChai;
