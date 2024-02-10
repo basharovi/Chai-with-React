6089f329385a7f0e9bfcc25d778b33991514a35f
Mominur Bashar Ovi <bashar.ovi@outlook.com>
2024-02-10 16:50:15 +0600

    added counter with state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Chai from "./Chai";
+import HeyChai from "./Chai";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <Chai />
+        <HeyChai />
       </header>
     </div>
   );
--- src/Chai.js
+++ src/Chai.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 function HeyChai() {
+  const [counter, setCounter] = useState(0);
+
+  const Increase = () => {
+    if (counter < 20) setCounter(counter + 1);
+  };
+
+  const Decrease = () => {
+    if (counter > 0) setCounter(counter - 1);
+  };
+
   return (
     <>
-      <h4 className="App-logo"> Hello React! </h4>
-      <p> This is a simple React app. </p>
+      <h4 className="App-logo"> {counter} </h4>
+      <div style={{ display: "inline-flex", gap: "5px" }}>
+        <button onClick={Increase}> Increase </button>
+        <button onClick={Decrease}> Decrease </button>
+      </div>
     </>
   );
 }
